0edea9e21e8f203888f648dc89b466c4efe7355c
Carlos <160207677+CarlosW-sky@users.noreply.github.com>
2024-07-16 22:48:50 -0600

    ThirdPersonController

---
+++ Visual Studio/Assets/ThirdPersonController/CameraController.cs
@@ -0,0 +1,68 @@
+﻿using System.Media;
+using UnityEngine;
+// using System;
+
+public class CameraController : MonoBehaviour
+{
+  
+
+    [SerializeField] Transform followTarget;
+                         // 旋转速度和 距离
+    [SerializeField] float rotationSpeed = 2f;
+    [SerializeField] float distance = 5;
+                             // 缩放速度 和 缩小距离 和 缩大距离 
+    [SerializeField] float zoomSpeed = 2f; // 缩放速度
+    [SerializeField] float minDistance = 2f; // 最小距离
+    [SerializeField] float maxDistance = 10f; // 最大距离
+             // 垂直角 小和大
+    [SerializeField] float minVerticalAngle = -45f;
+    [SerializeField] float maxVerticalAngle = 45;
+
+    [SerializeField] Vector2 framingOffset;
+
+    [SerializeField] bool invertX;
+    [SerializeField] bool invertY;
+    
+    float rotationX;
+    float rotationY;
+
+    float invertXal;
+    float invertYal;
+    private int invertXVal;
+    private int invertYVal;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+
+    private void Update()
+    {
+        invertXVal = (invertX) ? -1 : 1;
+        invertYVal = (invertY) ? -1 : 1;
+
+        rotationX += Input.GetAxis("Mouse Y") * invertYVal * rotationSpeed;
+        rotationX = Mathf.Clamp(rotationX, minVerticalAngle, maxVerticalAngle);
+
+        rotationY += Input.GetAxis("Mouse X") * invertXVal * rotationSpeed;
+
+        var targetRotation = Quaternion.Euler(rotationX, rotationY, 0);
+
+        var focusPostion = followTarget.position + new Vector3(framingOffset.x,framingOffset.y);
+
+        
+        // 获取鼠标滚轮输入并调整距离
+        float scrollInput = Input.GetAxis("Mouse ScrollWheel");
+        distance -= scrollInput * zoomSpeed;
+        distance = Mathf.Clamp(distance, minDistance, maxDistance);
+
+
+        transform.position = focusPostion - targetRotation * new Vector3(0, 0, distance);
+        transform.rotation = targetRotation;
+
+    }
+        public Quaternion PlanarRotation => Quaternion.Euler(0, rotationY, 0);
+}
---
+++ Visual Studio/Assets/ThirdPersonController/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿/*using System.Collections;
+using System.Collections.Generic; */
+using UnityEngine;
+using System;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float rotationSpeed = 500f;
+    public float gravity = 1f;
+
+    Quaternion targetRotation;
+
+    CameraController cameraController;
+
+    private void Awake()
+    {
+        cameraController = Camera.main.GetComponent<CameraController>(); // 获取主相机的 Transform 组件
+    }
+    private void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        float moveAmount = Mathf.Abs(h) + Mathf.Abs(v);
+
+        var moveInput = (new Vector3(h, 0, v)).normalized;
+
+        var moveDir = cameraController.PlanarRotation * moveInput;
+
+        if (moveAmount > 0)
+        {
+            transform.position += moveDir * moveSpeed * Time.deltaTime;
+            targetRotation = Quaternion.LookRotation(moveDir);
+        }
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation,
+            rotationSpeed * Time.deltaTime);
+    }
+}
